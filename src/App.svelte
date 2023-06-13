66e795927cb807846d0467db740679a67423d4cd
Yue Jiao <cleamoon@hotmail.com>
2023-06-13 22:06:16 +0200

    Add file path similar to ifraviewer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts" context="module">
-  import { filesInCurrentFolder, fileUrl, imageStyle } from "./lib/stores.js";
+  import { filesInCurrentFolder, fileUrl, file, imageStyle } from "./lib/stores.js";
   import { openFile, openFileByPath } from "./lib/FileManaging.svelte";
   import { handleKeydown, nextFile } from "./lib/Shortcuts.svelte";
   import { invoke } from "@tauri-apps/api/tauri";
@@ -18,6 +18,9 @@
   {#if $filesInCurrentFolder.length > 0}
     <img src={$fileUrl} alt={$fileUrl} style={$imageStyle} />
   {/if}
+  {#if $filesInCurrentFolder.length > 0}
+    <div class="info">{$file?.path}</div>
+  {/if}
 </main>
 
 <svelte:window on:keydown={(event) => handleKeydown(event)} on:click={nextFile} />
@@ -26,16 +29,29 @@
   main {
     margin: 0;
     padding: 0;
-    height: 98vh;
-    width: 98vw;
+    height: 99vh;
+    width: 99vw;
+    align-items: center;
+    justify-content: center;
     display: block;
     background-color: #eeeeee;
-    overflow: scroll;
+    overflow: hidden;
     user-select: none;
     -webkit-user-select: none;
     position: relative;
     overscroll-behavior-y: none;
     overscroll-behavior-x: none;
     cursor: grab;
+  }
+
+  div.info {
+    position: absolute;
+    top: auto;
+    bottom: 0;
+    left: auto;
+    right: 0;
+    font-size: 2;
+    color: rgba(50, 255, 50, 1);
+    background-color: rgba(10, 10, 10, 0.2);
   }
 </style>
