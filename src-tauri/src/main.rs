342f55f22fddb506189fb80d4353ad774bb3914a
Yue Jiao <cleamoon@hotmail.com>
2023-10-02 09:46:41 +0200

    Return full path from rust command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,29 @@
     windows_subsystem = "windows"
 )]
 
-use std::env;
+use std::{env, fs};
 
 #[tauri::command]
-fn get_args() -> Vec<String> {
-    env::args().collect()
+fn get_full_path() -> String {
+    let default_path = String::from("");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        return default_path;
+    }
+    let file_path = &args[1];
+
+    let full_path = match fs::canonicalize(file_path) {
+        Ok(full_path) => return full_path.display().to_string(),
+        Err(_) => default_path,
+    };
+
+    return full_path;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_args])
+        .invoke_handler(tauri::generate_handler![get_full_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
